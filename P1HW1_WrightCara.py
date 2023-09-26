52f04985f7716da80b3d04a9c53c1a90faa157ab
sanderfc7183 <146065720+sanderfc7183@users.noreply.github.com>
2023-09-25 21:44:39 -1000

    Add files via upload
    Second Submission

---
+++ P1HW1_WrightCara.py
@@ -0,0 +1,19 @@
+
+user_num = int(input("Enter integer:\n"))
+
+print("You entered:", user_num)
+
+
+square = user_num * user_num
+cube = user_num * user_num * user_num
+print(user_num, "squared is", square)
+print("And", user_num, "cubed is", cube, "!!")
+
+
+user_num2 = int(input("Enter another integer:\n"))
+
+sum_result = user_num + user_num2
+product_result = user_num * user_num2
+print(user_num, "+", user_num2, "is", sum_result)
+print(user_num, "*", user_num2, "is", product_result)
+
